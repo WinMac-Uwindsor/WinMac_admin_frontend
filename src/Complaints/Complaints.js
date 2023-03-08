b32de63d8a6e53b9d8952e41e343f2d08287afa6
MeetOdedara <meedodedara123@gmail.com>
2023-03-08 17:35:10 -0500

    modal for update events

--- src/Complaints/Complaints.js
+++ src/Complaints/Complaints.js
@@ -1,9 +1,5 @@
 import Button from "@mui/material/Button";
-<<<<<<< HEAD
-import './Complaints.css';
-=======
 import "./Complaints.css";
->>>>>>> dc66b840d6e9f1fd65b1eacfa93bc824d1bc13ce
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
@@ -11,14 +7,9 @@ import axios from "axios";
 import Typography from "@mui/material/Typography";
 import React, { useState, useEffect } from "react";
 
-<<<<<<< HEAD
-
-export default function Complaints() {
-=======
 export default function Complaints() {
   const username = localStorage.getItem('username');
 
->>>>>>> dc66b840d6e9f1fd65b1eacfa93bc824d1bc13ce
   const [details, setDetails] = useState([]);
 
   useEffect(() => {
@@ -26,21 +17,6 @@ export default function Complaints() {
   }, []);
 
   function getComplsints() {
-<<<<<<< HEAD
-      axios.get("http://localhost:5000/winmac/support/").then((response) => {
-      setDetails(response.data);
-      console.log("response: "+response.data);
-    });
-  }
-
-
-  function deleteComplaint(id) {
-    console.log("id: "+id);
-    axios
-      .post("http://localhost:5000/winmac/support/deleteTicket", {'event_id': id})
-      .then((response) => {
-        console.log("cancel success",response.data);
-=======
     axios.get("http://localhost:5000/winmac/support/").then((response) => {
       setDetails(response.data);
       console.log("response: " + response.data);
@@ -55,7 +31,6 @@ export default function Complaints() {
       })
       .then((response) => {
         console.log("cancel success", response.data);
->>>>>>> dc66b840d6e9f1fd65b1eacfa93bc824d1bc13ce
         getComplsints();
       })
       .catch((error) => {
@@ -63,49 +38,11 @@ export default function Complaints() {
       });
   }
 
-<<<<<<< HEAD
-  
-  console.log("length: "+details.length);
-=======
   console.log("length: " + details.length);
->>>>>>> dc66b840d6e9f1fd65b1eacfa93bc824d1bc13ce
   console.log("Details: " + JSON.stringify(details));
 
   return (
     <div>
-<<<<<<< HEAD
-      <br/>
-    {details.length > 0 &&
-      details.data.map((item, index) => (
-        <Card sx={{ maxWidth: 700 }} className="event" key={index}>
-          <CardHeader 
-            subheader={"Ticket No.: "+item._id}
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {"Student username: "+item.username}<br/>
-              {"Complaint: "+item.message}
-            </Typography>
-          </CardContent>{" "}
-          
-            <div>
-            <Button
-              onClick={() => deleteComplaint(item._id)}
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Cancel
-            </Button>
-            </div>
-          
-        </Card>
-      ))}
-    </div>
-  );
-}
-             
-=======
       <br />
       {details.length > 0 &&
         details.data.map((item, index) => (
@@ -133,4 +70,3 @@ export default function Complaints() {
     </div>
   );
 }
->>>>>>> dc66b840d6e9f1fd65b1eacfa93bc824d1bc13ce
--- src/Events/Events.js
+++ src/Events/Events.js
@@ -7,29 +7,48 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import UpdateEvent from "./UpdateEvent";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
+import Box from "@mui/material/Box";
 
+import Modal from "@mui/material/Modal";
 
-const Events = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const Events = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const navigate = useNavigate();
   function handleGenerateQR(eventId) {
     navigate(`/QRGenerator/${eventId}`);
   }
- 
+
   function handleUpdate() {
     navigate(`/UpdateEvent`);
   }
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState('');
-  const [limit, setLimit] = useState('');
-  const [description, setDescription] = useState('');
-  const [author, setAuthor] = useState('');
-  const [location, setLocation]= useState(''); 
-  const [date, setDate] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
-
+  const [title, setTitle] = useState("");
+  const [limit, setLimit] = useState("");
+  const [description, setDescription] = useState("");
+  const [author, setAuthor] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
 
   useEffect(() => {
     getEvents();
@@ -42,186 +61,209 @@ const Events = () => {
     });
   }
 
-  
   function deleteEvent(event) {
-    console.log("eventid: "+event);
-    const url = 'http://localhost:5000/winmac/eventList/deleteEvent';
-    const data = { event_id: event};
-    
-    axios.post(url, data)
-      .then(response => {
+    console.log("eventid: " + event);
+    const url = "http://localhost:5000/winmac/eventList/deleteEvent";
+    const data = { event_id: event };
+
+    axios
+      .post(url, data)
+      .then((response) => {
         console.log(response.data);
         getEvents();
       })
-      .catch(error => {
-        console.error("This is error: ",error);
+      .catch((error) => {
+        console.error("This is error: ", error);
       });
   }
 
   console.log("data: ", data.data);
   function handleFormSubmit(event) {
     event.preventDefault();
-    console.log(event.target)
-    console.log(title+"\n"+description+"\n"+author+"\n"+limit+"\n"+location+"\n"+date+"\n"+startTime+"\n"+endTime);
-    setTitle('');
-    setDescription('');
-    setAuthor('');
-    setLocation('');
-    setStartTime('');
-    setEndTime('');
-  
+    console.log(event.target);
+    console.log(
+      title +
+        "\n" +
+        description +
+        "\n" +
+        author +
+        "\n" +
+        limit +
+        "\n" +
+        location +
+        "\n" +
+        date +
+        "\n" +
+        startTime +
+        "\n" +
+        endTime
+    );
+    setTitle("");
+    setDescription("");
+    setAuthor("");
+    setLocation("");
+    setStartTime("");
+    setEndTime("");
   }
-  
+
   return (
     <div className="event">
-   
       <form className="form" onSubmit={handleFormSubmit}>
-          <TextField
-          
-            label="Title"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-            required
-          />
-          <TextField
-            label="Description"
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-            
-            required
-          />
-          <TextField
-            label="Author"
-            value={author}
-            onChange={(event) => setAuthor(event.target.value)}
-            multiline
-            required
-          />
-          <TextField
-            label="Limit"
-            type="number"
-  pattern="[0-9]*"
-            value={limit}
-            onChange={(event) => setLimit(event.target.value)}
-            
-            required
-          />
-          <TextField
-            label="Location"
-            value={location}
-            onChange={(event) => setLocation(event.target.value)}
-            multiline
-            required
-          />
-          <TextField
-            name="date"
-            label="Date"
-            type="date"
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            name="startTime"
-            label="Start Time"
-            type="time"
-            value={startTime}
-            onChange={(event) => setStartTime(event.target.value)}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            inputProps={{
-              step: 300,
-            }}
-          />
-          <TextField
-            name="endTime"
-            label="End Time"
-            type="time"
-            value={endTime}
-            onChange={(event) => setEndTime(event.target.value)}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            inputProps={{
-              step: 300,
-            }}
-          />
+        <TextField
+          label="Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          required
+        />
+        <TextField
+          label="Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          required
+        />
+        <TextField
+          label="Author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          multiline
+          required
+        />
+        <TextField
+          label="Limit"
+          type="number"
+          pattern="[0-9]*"
+          value={limit}
+          onChange={(event) => setLimit(event.target.value)}
+          required
+        />
+        <TextField
+          label="Location"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          multiline
+          required
+        />
+        <TextField
+          name="date"
+          label="Date"
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          name="startTime"
+          label="Start Time"
+          type="time"
+          value={startTime}
+          onChange={(event) => setStartTime(event.target.value)}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          inputProps={{
+            step: 300,
+          }}
+        />
+        <TextField
+          name="endTime"
+          label="End Time"
+          type="time"
+          value={endTime}
+          onChange={(event) => setEndTime(event.target.value)}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          inputProps={{
+            step: 300,
+          }}
+        />
 
-          
-          <Button type="submit" variant="contained" color="primary" >
-            Submit
-          </Button>
-          
-                
-              
-        </form>
-        
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+
       <div>
-      <br/>
-      {data.length > 0 &&
-        data.data.map((item, index) => (
-          <Card sx={{ maxWidth: 700 }} className="event" key={index}>
-            <CardHeader
-              title={item.title}
-              subheader={"By: " + item.Presenter}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                Date of Event: {item.title}
-                <br />
-                Time of Event: {item.time}
-                <br />
-                Location of Event: {item.location}
-                <br />
-                Event Description: {item.Desc}
-                Event Date: {item.date}
-                Event startTime: {item.startTime}
-                Event end time: {item.endTime}
-
-
-              </Typography>
-            </CardContent>{" "}
-            {
-              <div className="eventBtn" >
-              <Button
-                onClick={() => deleteEvent(item.event_id)}
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Delete
-              </Button>{" "}
-              <Button
-                  onClick={()=>handleUpdate()}
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Update
-                </Button>{" "}
-              <Button
-                  onClick={()=>handleGenerateQR(`${item.event_id}`)}
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Generate QR
-                </Button>
-            </div>
-            }
-          </Card>
-        ))}
+        <br />
+        {data.length > 0 &&
+          data.data.map((item, index) => (
+            <Card sx={{ maxWidth: 700 }} className="event" key={index}>
+              <CardHeader
+                title={item.title}
+                subheader={"By: " + item.Presenter}
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  Date of Event: {item.title}
+                  <br />
+                  Time of Event: {item.time}
+                  <br />
+                  Location of Event: {item.location}
+                  <br />
+                  Event Description: {item.Desc}
+                  Event Date: {item.date}
+                  Event startTime: {item.startTime}
+                  Event end time: {item.endTime}
+                </Typography>
+              </CardContent>{" "}
+              {
+                <div className="eventBtn">
+                  <Button
+                    onClick={() => deleteEvent(item.event_id)}
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Delete
+                  </Button>{" "}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleOpen}
+                  >
+                    Open modal
+                  </Button>
+                  <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    slots={{ backdrop: Backdrop }}
+                    slotProps={{
+                      backdrop: {
+                        timeout: 500,
+                      },
+                    }}
+                  >
+                    <Fade in={open}>
+                      <Box sx={style}>
+                        <UpdateEvent />
+                      </Box>
+                    </Fade>
+                  </Modal>
+                  <Button
+                    onClick={() => `${item.event_id}`}
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Generate QR
+                  </Button>
+                </div>
+              }
+            </Card>
+          ))}
       </div>
-        
     </div>
   );
 };
